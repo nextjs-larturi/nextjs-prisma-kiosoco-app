a376b630fded8db4cd46b402d1903957d35eda87
Leandro Arturi <lea.arturi@gmail.com>
2022-03-23 12:56:26 -0300

    Graba orden en BD

--- context/KioscoProvider.jsx
+++ context/KioscoProvider.jsx
@@ -1,91 +1,129 @@
 import { useState, useEffect, createContext } from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const KioscoContext = createContext();
 
-const KioscoProvider = ({children}) => {
+const KioscoProvider = ({ children }) => {
+   const [categorias, setCategorias] = useState([]);
+   const [categoriaActual, setCategoriaActual] = useState({});
+   const [producto, setProducto] = useState({});
+   const [modal, setModal] = useState(false);
+   const [carrito, setCarrito] = useState([]);
+   const [nombre, setNombre] = useState('');
+   const [total, setTotal] = useState(0);
 
-    const [categorias, setCategorias] = useState([]);
-    const [categoriaActual, setCategoriaActual] = useState({});
-    const [producto, setProducto] = useState({});
-    const [modal, setModal] = useState(false);
-    const [carrito, setCarrito] = useState([]);
+   const router = useRouter();
 
-    const getCategorias = async () => {
-        const { data } = await axios.get('/api/categorias');
-        setCategorias(data);
-    }
+   const getCategorias = async () => {
+      const { data } = await axios.get('/api/categorias');
+      setCategorias(data);
+   };
 
-    useEffect(() => {
-        getCategorias();
-    }, []);
+   useEffect(() => {
+      getCategorias();
+   }, []);
 
-    useEffect(() => {
-        setCategoriaActual(categorias[0]);
-    }, [categorias]);
+   useEffect(() => {
+      const nuevoTotal = carrito.reduce((total, producto) => {
+         return total + producto.precio * producto.cantidad;
+      }, 0);
+      setTotal(nuevoTotal);
+   }, [carrito]);
 
-    const handleClickCategoria = (id) => {
-        const categoria = categorias.filter(c => c.id === id);
-        setCategoriaActual(categoria[0]);
-    };
+   useEffect(() => {
+      setCategoriaActual(categorias[0]);
+   }, [categorias]);
 
-    const handleSetProducto = (producto) => {
-        setProducto(producto);
-    };
+   const handleClickCategoria = (id) => {
+      const categoria = categorias.filter((c) => c.id === id);
+      setCategoriaActual(categoria[0]);
+      router.push('/');
+   };
 
-    const handleSetModal = (modal) => {
-        setModal(!modal);
-    };
+   const handleSetProducto = (producto) => {
+      setProducto(producto);
+   };
 
-    const handleClickAgregarCarrito = (producto) => {
-        if(carrito.some(p => p.id === producto.id)) {
-            // Actualiza la cantidad del producto
-            const productoActual = carrito.map(productoState => productoState.id === producto.id ? producto : productoState);
-            setCarrito(productoActual);
-            toast.success(`Modificaste: ${producto.nombre}`);
-        } else {
-            setCarrito([ ...carrito, producto ]);
-            toast.success(`Agregaste: ${producto.nombre}`);
-        }
+   const handleSetModal = (modal) => {
+      setModal(!modal);
+   };
 
-        setModal(false);
-    };
+   const handleClickAgregarCarrito = (producto) => {
+      if (carrito.some((p) => p.id === producto.id)) {
+         // Actualiza la cantidad del producto
+         const productoActual = carrito.map((productoState) =>
+            productoState.id === producto.id ? producto : productoState
+         );
+         setCarrito(productoActual);
+         toast.success(`Modificaste: ${producto.nombre}`);
+      } else {
+         setCarrito([...carrito, producto]);
+         toast.success(`Agregaste: ${producto.nombre}`);
+      }
 
-    const handleEditarCantidad = (id) => {
-        const productoActualizar = carrito.filter(producto => producto.id === id);
-        setProducto(productoActualizar[0]);
-        setModal(!modal);
-    };
+      setModal(false);
+   };
 
-    const handleEliminarProducto = (id) => {
-        const carritoActualizado = carrito.filter(producto => producto.id !== id);
-        setCarrito(carritoActualizado);
-    };
+   const handleEditarCantidad = (id) => {
+      const productoActualizar = carrito.filter(
+         (producto) => producto.id === id
+      );
+      setProducto(productoActualizar[0]);
+      setModal(!modal);
+   };
 
-    return (
-        <KioscoContext.Provider
-            value={{
-                categorias,
-                producto,
-                modal,
-                carrito,
-                categoriaActual,
-                handleClickCategoria,
-                handleSetProducto,
-                handleSetModal,
-                handleClickAgregarCarrito,
-                handleEditarCantidad,
-                handleEliminarProducto,
-            }}
-        >
-            {children}
-        </KioscoContext.Provider>
-    )
-}
+   const handleEliminarProducto = (id) => {
+      const carritoActualizado = carrito.filter(
+         (producto) => producto.id !== id
+      );
+      setCarrito(carritoActualizado);
+   };
 
-export {
-    KioscoProvider,
-}
+   const colocarOrden = async (e) => {
+      e.preventDefault();
+
+      console.log(111);
+      try {
+         const { data } = await axios.post('/api/ordenes', {
+            pedido: carrito,
+            nombre,
+            total,
+            fecha: Date.now().toString(),
+         });
+
+         console.log(data);
+      } catch (error) {
+         console.error(error);
+      }
+   };
+
+   return (
+      <KioscoContext.Provider
+         value={{
+            categorias,
+            producto,
+            modal,
+            carrito,
+            categoriaActual,
+            nombre,
+            handleClickCategoria,
+            handleSetProducto,
+            handleSetModal,
+            handleClickAgregarCarrito,
+            handleEditarCantidad,
+            handleEliminarProducto,
+            setNombre,
+            colocarOrden,
+            total,
+         }}
+      >
+         {children}
+      </KioscoContext.Provider>
+   );
+};
+
+export { KioscoProvider };
 
 export default KioscoContext;
--- pages/total.jsx
+++ pages/total.jsx
@@ -1,15 +1,62 @@
-import Layout from "../layout/Layout";
+import { useEffect, useCallback } from 'react';
+import useKiosco from '../hooks/useKiosco';
+import Layout from '../layout/Layout';
+import { formatearMoneda } from '../helpers';
 
 export default function Total() {
-    return (
-     <Layout pagina="Total y Confirmar Pedido">
-          <h1 className="text-4xl font-black">
-            Total y Confirmar Pedido
-          </h1>
+   const { carrito, nombre, total, setNombre, colocarOrden } = useKiosco();
 
-          <p className="text-2xl my-10">
-            Confirma tu pedido
-          </p>
-     </Layout>
-    )
+   const validarFormulario = useCallback(() => {
+      return carrito.length === 0 || nombre.trim() === '';
+   }, [carrito, nombre]);
+
+   useEffect(() => {
+      validarFormulario();
+   }, [carrito, validarFormulario]);
+
+   return (
+      <Layout pagina='Total y Confirmar Pedido'>
+         <h1 className='text-4xl font-black'>Total y Confirmar Pedido</h1>
+
+         <p className='text-2xl my-10'>Confirma tu pedido</p>
+
+         <form onSubmit={colocarOrden}>
+            <div>
+               <label
+                  htmlFor='nombre'
+                  className='block uppercase text-slate-800 font-bold text-xl'
+               >
+                  Nombre
+               </label>
+               <input
+                  type='text'
+                  id='nombre'
+                  className='bg-gray-200 w-full lg:w-1/3 p-2 rounded-md mt-3'
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+               />
+            </div>
+
+            <div className='mt-10'>
+               <p className='text-2xl'>
+                  Total a pagar{' '}
+                  <span className='font-bold'>{formatearMoneda(total)}</span>
+               </p>
+            </div>
+
+            <div className='mt-5'>
+               <input
+                  type='submit'
+                  className={`${
+                     validarFormulario()
+                        ? 'bg-indigo-100'
+                        : 'bg-indigo-600 hover:bg-indigo-800 cursor-pointer'
+                  }  w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
+                  value={'Confirmar Pedido'}
+                  disabled={validarFormulario()}
+               />
+            </div>
+         </form>
+      </Layout>
+   );
 }
